e1152ed7e5cf4a91aaa469f352961ab013485c87
SUMAYAGHANEM <sumaghanem9@gmail.com>
2021-03-15 04:42:22 -0700

    finished

---
+++ js/main.js
@@ -0,0 +1,112 @@
+'use strict';
+
+let yourName = prompt('what is you name?');
+alert('welcome' + ' ' + yourName);
+
+let mySport = prompt('is basketball my favorite sport?');
+if (mySport === 'yes')
+{
+  alert('you are correct!');
+}
+else
+{
+  alert('you are worng!');
+}
+// if (mySport === 'yes')
+// {
+//   console.log();('you are correct!');
+// }
+// else
+// {
+//   console.log();('you are worng!');
+// }
+
+
+let myAge = prompt('Am I 24 years old?');
+if (myAge === 'yes')
+{
+  alert('that right');
+}
+else
+{
+  alert('thats not right!');
+}
+
+// let myAge = prompt('Am I 24 years old?');
+// if (myAge === 'yes')
+// {
+//   console.log();('that right');
+// }
+// else
+// {
+//   console.log();('thats not right!');
+// }
+
+
+
+let myNickNAme = prompt('is my favourit nick name semsem ?');
+if (myNickNAme === 'no')
+{
+  alert('that right,its suma :)');
+}
+else
+{
+  alert('thats not right, its suma!');
+}
+
+
+// let myNickNAme = prompt('is my favourit nick name semsem ?');
+// if (myNickNAme === 'no')
+// {
+//   console.log();('that right,its suma :)');
+// }
+// else
+// {
+//   console.log();('thats not right, its suma!');
+// }
+
+let myHobby = prompt('is drawing one of my hobbies?');
+if (myHobby === 'yes')
+{
+  alert('that right,im a good painter :)');
+}
+else
+{
+  alert('thats not right');
+}
+
+// let myHobby = prompt('is drawing one of my hobbies?');
+// if (myHobby === 'yes')
+// {
+//   console.log();('that right,im a good painter :)');
+// }
+// else
+// {
+//   console.log();('thats not right');
+// }
+
+let myHeight = prompt('is my height 160 cm ?');
+if (myHeight === 'yes')
+{
+  alert('Nooo, im 171 cm ');
+}
+else
+{
+  alert('your right, im 171 cm');
+}
+
+// let myHeight = prompt('is my height 160 cm ?');
+// if (myHeight === 'yes')
+// {
+//   console.log();('Nooo, im 171 cm ');
+// }
+// else
+// {
+//   console.log();('your right, im 171 cm');
+// }
+
+
+
+
+
+
